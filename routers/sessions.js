ce6d273323ccccf812c5edbcca44212840d071d5
A. Scott Tribble <a.scott.tribble@gmail.com>
2018-02-14 10:12:33 -0800

    not sure anything changed

--- routers/sessions.js
+++ routers/sessions.js
@@ -65,6 +65,34 @@ router.post('/', (req, res) => {
   });
 });
 
+router.put('/:id', (req, res)=>{
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id ` +
+      `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).json({ message: message });
+  }
+
+  const toUpdate = {};
+  const updateableFields = ['title', 'date', 'startTime', 'endTime', 'location', 'description', 'notes', 'attendees'];
+
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+  });
+
+  Session
+  // all key/value pairs in toUpdate will be updated -- that's what `$set` does
+  .findByIdAndUpdate(req.params.id, { $set: toUpdate })
+  .then(session => res.status(204).end())
+  .catch(err => res.status(500).json({ message: 'Internal server error' }));
+});
+
+
+
+
 module.exports = router;
 
 
